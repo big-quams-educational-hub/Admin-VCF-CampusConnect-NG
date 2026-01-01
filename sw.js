0b046adb2c9a7ae21e83db96dcdbf244a2b65240
Big Quams <bigquamsmedia024@gmail.com>
2026-01-01 11:17:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,10 @@
-const CACHE_NAME="cc-admin-v1";
-const ASSETS=[
-  "./",
-  "index.html",
-  "manifest.json"
-];
+const CACHE_NAME = 'campusconnect-admin-cache-v1';
+const urlsToCache = ['./index.html','manifest.json'];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS))
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request).then(fetchRes=>{
-        return caches.open(CACHE_NAME).then(cache=>{
-          cache.put(e.request,fetchRes.clone());
-          return fetchRes;
-        });
-      }).catch(()=>res);
-    })
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
-      
